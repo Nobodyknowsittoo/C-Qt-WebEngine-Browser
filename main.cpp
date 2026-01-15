ca78d268ba460e16a31bc9c5448d40befbe8def7
Nobdyknowsit <antmoh11@gmail.com>
2026-01-15 20:06:27 +0100

    Full Setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QWebEngineView>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWebEngineView view;
+    view.setUrl(QUrl("https://www.qt.io"));
+    view.resize(1024, 768);
+    view.show();
+
+    return app.exec();
+}
